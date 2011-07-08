eb3ed6eeb70f4902c1d9c3a6bc1405b1fda28097
Kevin Lau <darealcaffeine@gmail.com>
2011-07-07 21:19:34 -0400

    master master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrappyApp::Application.config.secret_token = '29d87066d7f0a3d23ffe969f187ee17079cbe7752f3ee8d4535baca3dd48f99ad99ea7387d310728833005309ca7b76a8d351a3bcb6c6a8e1bb7595f71df1b35'
